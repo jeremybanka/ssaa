5402bb4c2dfacb179eda740194d60fa2231ffd6e
Jeremy Banka <jeremybanka@gmail.com>
2021-04-14 08:52:28 -0700

    ➕ add gh-pages and webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const HtmlWebpackPlugin = require(`html-webpack-plugin`)
+const { CleanWebpackPlugin } = require(`clean-webpack-plugin`)
+
 module.exports = {
   entry: `./src/js/app.js`,
   output: {
     path: `${__dirname}/dist`,
     filename: `bundle.js`,
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: `TITLE`,
+      favicon: `src/images/favicon.png`,
+      template: `src/index.html`, // template file
+      filename: `index.html`, // output file
+      inject: true,
+    }),
+  ],
   module: {
     rules: [
       {
